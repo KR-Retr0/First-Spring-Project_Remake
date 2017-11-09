b56dc6d425b8192b1b77adae6822197d54a0066c
retr0 <nakagimatv.2@gmail.com>
2017-11-09 16:02:30 +0900

    second update

--- src/main/java/com/firstweb/UserController/LoginController.java
+++ src/main/java/com/firstweb/UserController/LoginController.java
@@ -1,18 +1,33 @@
 package com.firstweb.UserController;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+	@Autowired
+	HttpSession session;
+
 	@RequestMapping("/")
 	public String MainPage() {
 		return "/main";
 	}
-	
+
 	@RequestMapping("/join")
 	public String JoinPage() {
 		return "/join";
 	}
-	
+
+	@RequestMapping("/login_req")
+	public String Login(HttpServletRequest req) throws UnsupportedEncodingException{
+		session.setAttribute("id", req.getParameter("id"));
+		//session.getAttribute("id");
+		return "/main";
+	}
 }
--- src/main/webapp/WEB-INF/views/join.jsp
+++ src/main/webapp/WEB-INF/views/join.jsp
@@ -8,6 +8,14 @@
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
 <body>
+	<b1>회원가입</b1>
 	
+	<form action="/join_req" method="POST" onclick="return JoinCheck()">
+		ID : <input type="text" name="id" class="TextField" data-korea="아이디"><br />
+		PW : <input type="text" name="pw" class="TextField" data-korea="패스워드"><br />
+		PW 확인 : <input type="text" class="pwCheck"><br />
+		닉네임 : <input type="text" name="name" class="TextField" data-korea="닉네임"><br />
+		성별 : 
+	</form>
 </body>
 </html>
